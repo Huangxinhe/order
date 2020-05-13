6ce37fba46bda1ea4e3e8104bab1bf5e1d8afa60
huangxinhe3 <huangxinhe3@jd.com>
2020-05-14 00:00:06 +0800

    Feign远程调用

--- src/main/java/com/imooc/order/client/ProductClient.java
+++ src/main/java/com/imooc/order/client/ProductClient.java
@@ -1,7 +1,12 @@
 package com.imooc.order.client;
 
+import com.imooc.order.dataobject.ProductInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 /**
  * @ClassName ProductClient
@@ -14,4 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface ProductClient {
     @GetMapping("/msg")
     String productMsg();
+
+    @GetMapping("/product/listForOrder")
+    List<ProductInfo> listForOrder(List<String> productIdList);
 }
--- src/main/java/com/imooc/order/controller/ClientController.java
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -1,13 +1,18 @@
 package com.imooc.order.controller;
 
 import com.imooc.order.client.ProductClient;
+import com.imooc.order.dataobject.ProductInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @ClassName ClientController
@@ -28,5 +33,12 @@ public class ClientController {
         String response = productClient.productMsg();
         log.info("response={}", response);
         return response;
+    }
+
+    @GetMapping("/getProductList")
+    public String getProductList(){
+        List<ProductInfo>  productInfoList =productClient.listForOrder(Arrays.asList("164103465734242707"));
+        log.info("response={}",productInfoList);
+        return "OK";
     }
 }
